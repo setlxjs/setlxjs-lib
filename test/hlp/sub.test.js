7905e6daa4cdf78611b2b7ff60dd013cb3165f85
Hendrik Niemann <h.neverman@web.de>
2016-05-18 11:01:11 +0200

    migrate sub to immutable

--- test/hlp/sub.test.js
+++ test/hlp/sub.test.js
@@ -1,4 +1,6 @@
-const sub = require('../../dist/hlp/sub');
+import sub from '../../src/hlp/sub';
+import s from '../../src/hlp/s';
+import l from '../../src/hlp/l';
 
 describe('hlp/sub', () => {
   it('should substract two numbers', () => {
@@ -8,12 +10,13 @@ describe('hlp/sub', () => {
   });
 
   it('should substract two sets', () => {
-    sub(new Set([1, 3, 4]), new Set([3, 4, 9, 12])).should.eql(new Set([1]));
+    sub(s(1, 3, 4), s(3, 4, 9, 12)).should.eql(s(1));
+    sub(s(1, 3, 4), s(3, 4, 9, 12)).should.eql(s(1));
   });
 
   it('should throw on incompatible types', () => {
     (() => sub('str', 'str')).should.throw('\'"str" - "str"\' is undefined.');
     (() => sub(true, true)).should.throw('\'true - true\' is undefined.');
-    (() => sub([1, 2], [2, 3])).should.throw();
+    (() => sub(l(1, 2), l(2, 3))).should.throw();
   });
 });
